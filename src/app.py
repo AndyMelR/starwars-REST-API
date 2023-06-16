d8a152c8b525cdab643ac63d26bc2b73029fcbd5
Andrea Melero <ameleror@hotmail.es>
2023-06-16 16:34:47 +0000

    endpoints ok

--- src/app.py
+++ src/app.py
@@ -37,7 +37,7 @@ def sitemap():
     return generate_sitemap(app)
 
 
-@app.route('/user', methods=['GET'])
+@app.route('/users', methods=['GET'])
 def handle_hello():
     users = User.query.all()
     response_user = [user.serialize() for user in users]
@@ -48,8 +48,8 @@ def handle_hello():
     return jsonify(response_user), 200 
 
 
-@app.route('/user/<int:user_id>', methods=['GET'])
-def get_one_user(user_id):
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_specific_user(user_id):
     user = User.query.get(user_id)
     user = user.serialize()
     return jsonify(user), 200
@@ -67,31 +67,31 @@ def get_people():
 
 
 @app.route('/people/<int:people_id>', methods=['GET'])
-def get_one_character(people_id):
+def get_specific_character(people_id):
     character = People.query.get(people_id)
-    oneCharacter = character.serialize()
-    return jsonify(oneCharacter), 200
+    singleCharacter = character.serialize()
+    return jsonify(singleCharacter), 200
 
 
-@app.route('/planet', methods = ['GET'])
+@app.route('/planets', methods = ['GET'])
 def get_planet():
     planets = Planet.query.all()
-    response_planet = [planet.serialize() for planet in planets]
+    planet = [planet.serialize() for planet in planets]
     response_body = {
         "msg": "Planet list"
     }
 
-    return jsonify(response_planet), 200 
+    return jsonify(planet), 200 
 
 
-@app.route('/planet/<int:planet_id>', methods=['GET'])
-def get_one_planet(planet_id):
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_specific_planet(planet_id):
     planet = Planet.query.get(planet_id)
-    onePlanet = planet.serialize()
-    return jsonify(onePlanet), 200
+    singlePlanet = planet.serialize()
+    return jsonify(singlePlanet), 200
 
 
-@app.route('/<int:user_id>/favorite', methods = ['GET'])
+@app.route('/<int:user_id>/favorites', methods = ['GET'])
 def get_user_favorites(user_id):
     favorite = Favorite.query.filter_by(user_id = user_id).all()
     favorite_user = [favorite.serialize() for favorite in favorite]
@@ -99,50 +99,62 @@ def get_user_favorites(user_id):
     return jsonify(favorite_user), 200
 
 
-@app.route('/<int:user_id>/favorite/people/<int:people_id>', methods = ['GET'])
-def post_people_to_favorite(user_id, people_id): 
-    favorite = Favorite(user_id = user_id, people_id = people_id)
+
+
+@app.route('/users/<int:user_id>/favorite/planet/<int:planet_id>', methods=['POST'])
+def add_favorite_planet(user_id, planet_id):
+    favorite = Favorite(user_id=user_id, planet_id=planet_id)
     db.session.add(favorite)
     db.session.commit()
     response_body = {
-        "msg": "Personaje agregado a favoritos "
+        "msg": "Favorite planet added to user"
+    }
+    return jsonify(response_body), 200
+
+@app.route('/users/<int:user_id>/favorite/people/<int:people_id>', methods=['POST'])
+def add_favorite_people(user_id, people_id):
+    favorite = Favorite(user_id=user_id, people_id=people_id)
+    db.session.add(favorite)
+    db.session.commit()
+    response_body = {
+        "msg": "Favorite people added to user"
     }
     return jsonify(response_body), 200
 
 
-@app.route('/<int:user_id>/favorite/planet/<int:planet_id>', methods = ['GET'])
-def post_planet_to_favorite(user_id, planet_id): 
-    favorite = Favorite(user_id = user_id, planet_id = planet_id)
-    db.session.add(favorite)
-    db.session.commit()
-    response_body = {
-        "msg": "Planeta agregado a favoritos "
-    }
-    return jsonify(response_body), 200
+@app.route('/users/<int:user_id>/favorite/planet/<int:planet_id>', methods=['DELETE'])
+def delete_favorite_planet(user_id, planet_id):
+    favorite = Favorite.query.filter_by(user_id=user_id, planet_id=planet_id).first()
+    if favorite:
+        db.session.delete(favorite)
+        db.session.commit()
+        response_body = {
+            "msg": "Favorite planet deleted"
+        }
+        return jsonify(response_body), 200
+    else:
+        response_body = {
+            "msg": "Favorite planet not found"
+        }
+        return jsonify(response_body), 404
+
+@app.route('/users/<int:user_id>/favorite/people/<int:people_id>', methods=['DELETE'])
+def delete_favorite_people(user_id, people_id):
+    favorite = Favorite.query.filter_by(user_id=user_id, people_id=people_id).first()
+    if favorite:
+        db.session.delete(favorite)
+        db.session.commit()
+        response_body = {
+            "msg": "Favorite people deleted"
+        }
+        return jsonify(response_body), 200
+    else:
+        response_body = {
+            "msg": "Favorite people not found"
+        }
+        return jsonify(response_body), 404
 
 
-@app.route('/<int:user_id>/favorite/people/<int:people_id>', methods=['DELETE'])
-def delete_people(user_id, people_id): 
-    people_deleted = Favorite.query.filter_by(user_id=user_id, people_id=people_id).first()
-    db.session.delete(people_deleted)
-    db.session.commit()
-    
-    response_body = {
-        "msg": "Personaje borrado"
-    }
-    return jsonify(response_body), 200  
-
-
-@app.route('/<int:user_id>/favorite/planet/<int:planet_id>', methods=['DELETE'])
-def delete_planet(user_id, planet_id): 
-    planet_deleted = Favorite.query.filter_by(user_id=user_id, planet_id=planet_id).first()
-    db.session.delete(planet_deleted)
-    db.session.commit()
-    
-    response_body = {
-        "msg": "Planeta borrado!"
-    }
-    return jsonify(response_body), 200   
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
